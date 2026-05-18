8d417c058f6a5a659b95dd17ba732ecbd0c7eb97
Ishita <ishitasharma3135@gmail.com>
2026-05-18 12:23:15 +0530

    Initial commit

---
+++ src/main/java/com/example/MistralAI/MistralController.java
@@ -0,0 +1,21 @@
+package com.example.MistralAI;
+
+import org.springframework.ai.mistralai.MistralAiChatModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/mistralai")
+@CrossOrigin(origins = "http://localhost:3000")
+public class MistralController {
+    private MistralAiChatModel chatModel;
+
+    public MistralController(MistralAiChatModel chatModel){
+        this.chatModel= chatModel;
+    }
+    @GetMapping("/{message}")
+    public ResponseEntity<String> getResponse(@PathVariable String message){
+        String response=chatModel.call(message);
+        return ResponseEntity.ok(response);
+    }
+}
---
+++ src/main/java/com/example/MistralAI/Repository/ChatModelRepository.java
@@ -0,0 +1,7 @@
+package com.example.MistralAI.Repository;
+
+import com.example.MistralAI.Models.ChatModels;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatModelRepository extends JpaRepository<ChatModels, Long> {
+}
